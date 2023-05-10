55634017e2c7fbb987daaeac3ff288ad623f30cd
Ashish bundela <ashishbundela123@gmail.com>
2023-05-11 04:53:34 +0530

    new connectio mongodb

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 // mongoose.connect('mongodb://0.0.0.0/employeReviewSystem');
-const DB = 'mongodb+srv://ashishbundela123:4ROoiZ9VErQnhrDp@cluster0.cc5ew57.mongodb.net/EmployeeReview'
-
+//const DB = 'mongodb+srv://ashishbundela123:4ROoiZ9VErQnhrDp@cluster0.cc5ew57.mongodb.net/EmployeeReview'
+mongoose.connect =( process.env.MONGO_URI)
 // These set of line can be written in async await fashion, but I have followed the documentation. 
-mongoose.connect(DB).then(()=>{
-     console.log('connection successful');
- }).catch((err) => console.log("no connection " + err));
+// mongoose.connect(DB).then(()=>{
+//      console.log('connection successful');
+//  }).catch((err) => console.log("no connection " + err));
 
 const db = mongoose.connection;
 
